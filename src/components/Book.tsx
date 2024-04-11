9af7ce92e56308be18c4aa155dbb1d231a9e42d6
Surya Kumar <surya.kumar@klarna.com>
2024-04-11 20:56:11 +0200

    Book component design update

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,13 +1,16 @@
 import { BookProps } from "@/types";
+import Image from "next/image";
 import React from "react";
 
 const Book: React.FC<{ book: BookProps }> = ({ book }) => {
   const { title, author, description, image } = book;
   return (
-    <div className="bg-white rounded-lg shadow-md p-4">
-      <img
+    <div className="bg-gray-900 rounded-lg shadow-md p-4">
+      <Image
         src={image}
         alt={title}
+        width={100}
+        height={140} // Set the height of the image to 140
         className="w-32 h-40 object-cover rounded-md mx-auto mb-4"
       />
       <h2 className="text-xl font-semibold mb-2">{title}</h2>
